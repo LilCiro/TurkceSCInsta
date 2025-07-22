053b8bed1222fe7b9f96afbeb5621de49a5b7ff4
LilCiro <94236924+LilCiro@users.noreply.github.com>
2025-07-22 13:46:45 +0300

    Update StepperTableCell.h

--- src/Components/Prefs/StepperTableCell.h
+++ src/Components/Prefs/StepperTableCell.h
@@ -1,15 +1,16 @@
-#import <CepheiPrefs/CepheiPrefs.h>
-#include <Foundation/Foundation.h>
+#import <CepheiPrefs/CepheiPrefs.h> // CepheiPrefs kütüphanesini içe aktarır (Ayarlar paneli geliştirmek için kullanılır) ⚙️
+#include <Foundation/Foundation.h> // Temel nesne ve veri türlerini içeren Foundation kütüphanesini içe aktarır 📚
 
-#import "../../Manager.h"
-#import "../../Utils.h"
+#import "../../Manager.h" // Uygulamanın genel yöneticisi (Manager) sınıfını içe aktarır 🚀
+#import "../../Utils.h" // Yardımcı (Utils) fonksiyonları içeren sınıfı içe aktarır 🔧
 
-#import "StepperTableCell.h"
+#import "StepperTableCell.h" // Kendi tanımladığımız StepperTableCell başlık dosyasını içe aktarır 📱
 
+// SCIStepperTableCell arayüz tanımı: PSControlTableCell'den türetilmiştir
 @interface SCIStepperTableCell : PSControlTableCell
 
-@property (nonatomic, retain) UIStepper *control;
-
-@property (nonatomic, retain) NSString *titleTemplate;
+// Özellikler
+@property (nonatomic, retain) UIStepper *control; // Hücre içinde kullanılacak UIStepper kontrolü ➕➖
+@property (nonatomic, retain) NSString *titleTemplate; // Hücre başlığının şablonu (örneğin "%@ saniye") 📝
 
 @end
